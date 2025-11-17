ba76276bcf19193fd92025b22a1c95789e1eebc0
SebuBergman <basti.bergman@proton.me>
2025-11-17 19:26:29 +0200

    feat: added new aliases to my imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import path from "path";
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@api": path.resolve(__dirname, "./src/app/api")
+    },
+  },
   plugins: [react()],
 })
